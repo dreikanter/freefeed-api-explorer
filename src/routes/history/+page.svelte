2902ae461ff102cb61945e363b37fbf2d2fe1d45
Alex Musayev <alex.musayev@gmail.com>
2025-09-14 21:20:37 +0200

    Add bullets to the params list

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -139,7 +139,7 @@
             <!-- Parameters -->
             {#if Object.keys(selectedRequest.parameters).length > 0}
               <h6>Parameters Used:</h6>
-              <ul class="list-unstyled">
+              <ul>
                 {#each Object.entries(selectedRequest.parameters) as [key, value]}
                   <li class="mb-1">
                     <code class="text-primary">{key}</code>: <code class="bg-light px-1">{value}</code>
